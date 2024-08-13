ca233bbef8b99b14fb04e53a4c7bff6988b3813b
mokhiya <mohiyaxonuzokova@gmail.com>
2024-08-13 14:09:56 +0500

    create new class of student

---
+++ Student/student.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self, login, password, full_name, email):
+        self.login = login
+        self.password = password
+        self.full_name = full_name
+        self.email = email
+        self.status = False
+        self.groups = []
+        self.lessons = []
