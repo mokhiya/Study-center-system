4e2cad8c0eeee3a846241b1b9885d8f2bcc9bdc3
mokhiya <mohiyaxonuzokova@gmail.com>
2024-08-13 15:56:55 +0500

    add student gender

--- Student/student.py
+++ Student/student.py
@@ -1,9 +1,10 @@
 class Student:
-    def __init__(self, login, password, full_name, email):
+    def __init__(self, login, password, full_name, gender, email):
         self.login = login
         self.password = password
         self.full_name = full_name
         self.email = email
+        self.gender = gender
         self.status = False
         self.groups = []
         self.lessons = []
