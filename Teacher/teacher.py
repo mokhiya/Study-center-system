3937e08e5abb0ac8d26f6f69c01342bf927cdccd
mokhiya <mohiyaxonuzokova@gmail.com>
2024-08-13 17:26:53 +0500

    add gender to teacher

--- Teacher/teacher.py
+++ Teacher/teacher.py
@@ -1,10 +1,11 @@
 from file_manager import teacher_manager
 
 class Teacher:
-    def __init__(self, login, password, full_name, email):
+    def __init__(self, login, password, full_name, gender, email):
         self.login = login
         self.password = password
         self.full_name = full_name
+        self.gender = gender
         self.email = email
         self.status = False
         self.groups = []
