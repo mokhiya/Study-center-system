5232b991030b9430ae4fd3e0b10bd227c692029b
mokhiya <mohiyaxonuzokova@gmail.com>
2024-08-13 20:43:48 +0500

    add small changes

--- Admin/groupmanagement.py
+++ Admin/groupmanagement.py
@@ -4,24 +4,32 @@ from file_manager import group_manager
 
 
 class Group:
-    def __init__(self, name, subject, start, end, duration, description):
+    def __init__(self, name, subject, start, end, duration, weekdays, time, room, description):
         self.name = name
         self.subject = subject
         self.start = start
         self.end = end
         self.duration = duration
+        self.weekdays = weekdays
+        self.time = time
+        self.room = room
         self.description = description
+        self.teachers = []
         self.status = False
 
     def to_dict(self):
-        # Convert the group object to a dictionary for storage
         return {
             "name": self.name,
             "subject": self.subject,
             "start": self.start.strftime("%d.%m.%Y"),
             "end": self.end.strftime("%d.%m.%Y"),
             "duration": self.duration,
-            "description": self.description
+            "weekdays": self.weekdays,
+            "time": self.time,
+            "room": self.room,
+            "description": self.description,
+            "teachers": self.teachers,
+            "status": self.status
         }
 
 
@@ -30,6 +38,9 @@ def create_group():
     subject = input("Enter a subject name for the group: ").title().strip()
     start_date_str = input("Enter start date (ex: 18.03.2024): ")
     duration_months = int(input("Enter duration in months: "))
+    weekdays_str = input("Enter weekdays (ex: Monday, Wednesday, Friday): ").title().strip()
+    time_str = input("Enter time (ex: 13:00-15:00): ").strip()
+    room = input("Enter room number or room name (ex: Yandex room): ").strip()
     group_description = input("Enter a description of the group: ").capitalize().strip()
 
     # Parse the start date
@@ -39,7 +50,9 @@ def create_group():
     end_date = start_date + relativedelta(months=duration_months)
 
     # Create the Group object with the calculated end date
-    group = Group(group_name, subject, start_date, end_date, duration_months, group_description)
+    group = Group(group_name, subject, start_date,
+                  end_date, duration_months, weekdays_str,
+                  time_str, room, group_description)
 
     # Store the group data
     group_manager.add_data(group.to_dict())
