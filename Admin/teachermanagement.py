9aa29454872cedddb7131966d4f1686ffe98cb24
mokhiya <mohiyaxonuzokova@gmail.com>
2024-08-13 14:07:38 +0500

    add delete teacher function

--- Admin/teachermanagement.py
+++ Admin/teachermanagement.py
@@ -1,6 +1,7 @@
 from Teacher.teacher import Teacher
 from file_manager import teacher_manager
 
+
 def create_teacher():
     teacher_login = input("Create teacher's login: ").strip()
     teacher_password = input("Create teacher's password: ").strip()
@@ -9,5 +10,14 @@ def create_teacher():
 
     teacher = Teacher(teacher_login, teacher_password, teacher_name, teacher_email)
     teacher_manager.add_data(teacher.__dict__)
+    return True
 
 
+def delete_teacher():
+    teacher_login = input("Enter teacher's login to delete account: ").strip()
+    teacher_data = teacher_manager.read_data()
+    for teacher in teacher_data:
+        if teacher['login'] == teacher_login:
+            teacher_manager.delete_data(teacher)
+            return True
+        return False
