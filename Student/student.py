6635dad28c2f121ef4ee5b8c1f5748869076c8fb
mokhiya <mohiyaxonuzokova@gmail.com>
2024-08-14 13:23:54 +0500

    add doc part

--- Student/student.py
+++ Student/student.py
@@ -1,4 +1,16 @@
 class Student:
+    """
+    This function initializes a Student object with the provided login, password,
+    full name, gender, and email. It also sets the default status to False and
+    initializes an empty list for groups.
+
+    Args:
+        login (str): The login username for the student.
+        password (str): The password for the student's account.
+        full_name (str): The full name of the student.
+        gender (str): The gender of the student.
+        email (str): The email address of the student.
+    """
     def __init__(self, login, password, full_name, gender, email):
         self.login = login
         self.password = password
